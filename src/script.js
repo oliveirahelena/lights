af2d383fff283c026c4c26d48697d9662afa1318
Helena Oliveira <helena@informach.com.br>
2026-01-31 09:44:51 -0300

    add lights

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
 import * as THREE from "three"
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import GUI from "lil-gui"
+import { RectAreaLightHelper } from "three/examples/jsm/Addons.js"
+
+// Debug
+const gui = new GUI()
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl")
@@ -7,11 +12,91 @@ const canvas = document.querySelector("canvas.webgl")
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+/**
+ * Lights
+ */
+// bom pra performance
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientLight)
+
+gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9)
+directionalLight.position.set(1, 0.25, 0)
+scene.add(directionalLight)
+
+// bom pra performance
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.9)
+scene.add(hemisphereLight)
+
+// params: color, intensity, distance, decay
+const pointLight = new THREE.PointLight(0xff9000, 1.5, 10, 2)
+pointLight.position.set(1, -0.5, 1)
+scene.add(pointLight)
+
+// params: color, intensity, width, heigth
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 6, 1, 1)
+rectAreaLight.position.set(-1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight)
+
+// params: color, intensity, distance, angle, penumbre, decay
+const spotLight = new THREE.SpotLight(0x78ff00, 4.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
+spotLight.target.position.x = -0.75
+scene.add(spotLight.target)
+
+// Helpers
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const direcionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(direcionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight, 0.2)
+scene.add(spotLightHelper)
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight, 0.2)
+scene.add(rectAreaLightHelper)
+
+/**
+ * Objects
+ */
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
+
+// Objects
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 32, 32),
+  material
+)
+sphere.position.x = - 1.5
+
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(0.75, 0.75, 0.75),
+  material
+)
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+  material
+)
+torus.position.x = 1.5
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(5, 5),
+  material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(sphere, cube, torus, plane)
 
 // Sizes
 const sizes = {
@@ -54,35 +139,45 @@ window.addEventListener('dblclick', () => {
   }
 })
 
-// Camera
-// fov: campo de visualização: ângulo vertical em graus: ideal entre 45 e 75
-// aspect ratio: largura do renderer dividido pela altura do renderer
-// near: distância mínima que a câmera consegue enxergar o objeto
-// far: distância máxima que a câmera consegue enxergar o objeto
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-camera.lookAt(mesh.position)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  sphere.rotation.y = 0.1 * elapsedTime
+  cube.rotation.y = 0.1 * elapsedTime
+  torus.rotation.y = 0.1 * elapsedTime
+
+  sphere.rotation.x = 0.15 * elapsedTime
+  cube.rotation.x = 0.15 * elapsedTime
+  torus.rotation.x = 0.15 * elapsedTime
 
   // Update controls
   controls.update()
